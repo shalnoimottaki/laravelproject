c01445ac9b613c12c8b892385e8c41d4b0db9812
Shalnoi Mottaki <58374132+shalnoimottaki@users.noreply.github.com>
2023-12-06 20:14:32 +0100

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('fname')->nullable();
-            $table->boolean('isAdmin')->default(true);
+            $table->boolean('isAdmin')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
