3048996269a2d7cc2e213bf65c71fc6e411f991b
Shalnoi Mottaki <58374132+shalnoimottaki@users.noreply.github.com>
2023-12-06 20:17:27 +0100

    Update isAdmin.php

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -17,7 +17,7 @@ class isAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if(!Auth::user()->isAdmin)
+        if(Auth::user()->isAdmin)
         return $next($request);
         return redirect()->back();
     }
